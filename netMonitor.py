5c5002c14d589f7ad0fcccb53455ce77e7dd7218
delga <delga@VirtualBox.localdomain>
2021-07-14 19:50:44 +0200

    updated main, parse_args and ses and added des

--- netMonitor.py
+++ netMonitor.py
@@ -13,12 +13,10 @@ def parse_args():
 
     parser = argparse.ArgumentParser(prog="netMonitor.py")
 
-    parser.add_argument("-t","--test", help="esegue uno speedtest 'times' volte con frequenza 'frequency'",
-             nargs='*', type=int, metavar=('times', 'frequency'))
-    parser.add_argument("-p","--plot", help="mostra il grafico dei dati raccolti",
-            action="store_true")
-    parser.add_argument("-f","--forecast", help="esegue una previsione usando i dati raccolti",
-            nargs='*', type=float, metavar=('alpha', 'beta'))
+    parser.add_argument("-t","--test", help="esegue uno speedtest 'times' volte con frequenza 'rate'",
+            nargs=2, type=int, metavar=('times', 'rate'), default=[5, 180])
+    parser.add_argument("-f","--forecast", help="esegue una previsione usando l'alpha (e beta) specificato", 
+            nargs='+', type=float, metavar=('alpha', 'beta'), default=[0.75])
 
     args = parser.parse_args()
 
@@ -34,7 +32,7 @@ def test():
 
     #ottengo il miglior server disponibile
     s.get_best_server()
-    
+
     #eseguo lo speedtest
     s.download(threads=1)
     s.upload(threads=1)
@@ -47,7 +45,7 @@ def test():
     tramite il client di influxdb '''
 
 def write2Influx(client, measurement, data):
-    
+
     #salvo l'istante di scrittura
     local_time = datetime.datetime.now()
 
@@ -72,14 +70,14 @@ def write2Influx(client, measurement, data):
     il risultato in un dataframe '''
 
 def influx2DataFrame(client, query):
-    
+
     #ottengo il risultato della query e la trasformo in un dataframe
     points = client.query(query).get_points()
     tmp_dataframe = pd.DataFrame(points)
 
     #trasformo la colonna contenente il tempo nell'indice del dataframe
     datetime_index = pd.DatetimeIndex(pd.to_datetime(tmp_dataframe["time"]).values)
-    dataframe = tmp_dataframe.set_index(datetime_index.to_period(freq='3t'))
+    dataframe = tmp_dataframe.set_index(datetime_index.to_period(freq='t'))
     dataframe.drop('time', axis=1, inplace=True)
 
     return dataframe
@@ -88,9 +86,19 @@ def influx2DataFrame(client, query):
 
 ''' esegue una previsione usando il Simple Exponential Smoothing '''
 
-def ses(dataframe, alpha, nForecast):
+def ses(dataframe, alpha):
     fit = SimpleExpSmoothing(dataframe).fit(smoothing_level=alpha, optimized=False)
-    forecast = fit.forecast(nForecast).rename(r'$\alpha=' + str(alpha) + '$')
+    forecast = fit.forecast(1).rename(r'$\alpha=' + str(alpha) + '$')
+    forecast.plot(style='--', marker='o', color='red', legend=True)
+    plot.savefig('ping', format='pdf')
+
+'''----------------------------------------------------------------------------------------------'''
+
+''' esegue una previsione usando il Double Exponential Smoothing '''
+
+def des(dataframe, alpha, beta):
+    fit = Holt(dataframe, exponential=True).fit(smoothing_level=alpha, smoothing_trend=beta, optimized=False)
+    forecast = fit.forecast(2).rename("Exponential trend")
     forecast.plot(style='--', marker='o', color='red', legend=True)
     plot.show()
 
@@ -99,16 +107,55 @@ def ses(dataframe, alpha, nForecast):
 ''' funzione principale '''
 
 def main():
-    
+
     args = parse_args()
     print(args)
+    times = args.test.pop(0)
+    
+    if times <= 0:
+        print("times deve essere un intero maggiore di zero")
+        exit(-1)
 
-    '''client = InfluxDBClient("127.0.0.1", 8086, "alessio", "gestione21", "speedtest")
+    rate = args.test.pop(0)
+
+    if rate <= 0:
+        print("rate deve essere un intero maggiore di zero")
+        exit(-1)
+
+    alpha = args.forecast.pop()
+    if alpha > 1: 
+        print('alpha deve essere compreso tra 0 e 1')
+        exit(-1)
+
+    beta = 0
+    if len(args.forecast) > 0:
+        beta = args.forecast.pop()
+        if beta > 1: 
+            print('beta deve essere compreso tra 0 e 1')
+            exit(-1)
+
+    user = input('Inserire lo username di influxdb: ')
+    password = input('Inserire la password di influxdb: ')
+    database_name = input('Inserire il nome del database di influxdb: ')
+
+    client = InfluxDBClient(username=user, password=password, database=database_name)
+
+    client.drop_measurement("speedtest")
+
+    for i in range(times):
+        init_time = time.time()
+        results = test()
+        write2Influx(client, "speedtest", results)
+        final_time = time.time()
+        time.sleep(rate - (final_time - init_time))
+
     dataframe = influx2DataFrame(client, 'select ping from speedtest')
     print(dataframe)
     dataframe.plot.line()
-    ses(dataframe, 0.2, 3) '''
-    
+    if beta == 0:
+        ses(dataframe, alpha)
+    else:
+        des(dataframe, alpha, beta)
 
 
 if __name__ == '__main__':
