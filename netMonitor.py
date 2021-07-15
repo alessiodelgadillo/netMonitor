9674940b48f2c7752bbf6855d4048e04d9c17d60
delga <delga@VirtualBox.localdomain>
2021-07-15 21:00:13 +0200

    updated main, ses, des and parse_args and added create_graphs

--- netMonitor.py
+++ netMonitor.py
@@ -1,20 +1,25 @@
 import argparse
 import time 
-import datetime
+import os
 import pandas as pd
 import matplotlib.pyplot as plot
+from datetime import datetime 
 from speedtest import Speedtest
 from influxdb import InfluxDBClient
 from statsmodels.tsa.api import SimpleExpSmoothing, Holt
 
-""" legge le opzioni e gli argomenti passati da linea di comando """
+
+GRAPHICS_PATH = './graphics'
+
+
+''' legge le opzioni e gli argomenti passati da linea di comando '''
 
 def parse_args():
 
     parser = argparse.ArgumentParser(prog="netMonitor.py")
 
-    parser.add_argument("-t","--test", help="esegue uno speedtest 'times' volte con frequenza 'rate'",
-            nargs=2, type=int, metavar=('times', 'rate'), default=[5, 180])
+    parser.add_argument("-t","--test", help="esegue uno speedtest 'times' volte",
+            nargs=1, type=int, metavar='times', default=5)
     parser.add_argument("-f","--forecast", help="esegue una previsione usando l'alpha (e beta) specificato", 
             nargs='+', type=float, metavar=('alpha', 'beta'), default=[0.75])
 
@@ -24,8 +29,7 @@ def parse_args():
 
 '''----------------------------------------------------------------------------------------------'''
 
-""" esegue lo speedtest sia in download e ritorna
-    un dizionario contenente i risultati """
+''' esegue lo speedtest sia in download e ritorna un dizionario contenente i risultati '''
 
 def test():
     s = Speedtest()
@@ -41,13 +45,12 @@ def test():
 
 '''----------------------------------------------------------------------------------------------'''
 
-'''  scrive i risultati di uno speedtest nella tabella measurement 
-    tramite il client di influxdb '''
+''' scrive i risultati di uno speedtest nella tabella measurement tramite il client di influxdb '''
 
 def write2Influx(client, measurement, data):
 
     #salvo l'istante di scrittura
-    local_time = datetime.datetime.now()
+    local_time = datetime.now()
 
     #genero il punto da inserire nel db
     point = [
@@ -66,8 +69,7 @@ def write2Influx(client, measurement, data):
 
 '''----------------------------------------------------------------------------------------------'''
 
-''' esegue una query sul database e trasforma
-    il risultato in un dataframe '''
+''' esegue una query sul database e trasforma il risultato in un dataframe '''
 
 def influx2DataFrame(client, query):
 
@@ -77,7 +79,7 @@ def influx2DataFrame(client, query):
 
     #trasformo la colonna contenente il tempo nell'indice del dataframe
     datetime_index = pd.DatetimeIndex(pd.to_datetime(tmp_dataframe["time"]).values)
-    dataframe = tmp_dataframe.set_index(datetime_index.to_period(freq='t'))
+    dataframe = tmp_dataframe.set_index(datetime_index.to_period(freq='3t'))
     dataframe.drop('time', axis=1, inplace=True)
 
     return dataframe
@@ -88,9 +90,10 @@ def influx2DataFrame(client, query):
 
 def ses(dataframe, alpha):
     fit = SimpleExpSmoothing(dataframe).fit(smoothing_level=alpha, optimized=False)
-    forecast = fit.forecast(1).rename(r'$\alpha=' + str(alpha) + '$')
+    forecast = fit.forecast(1).rename('Simple Exp Smoothing')
     forecast.plot(style='--', marker='o', color='red', legend=True)
-    plot.savefig('ping', format='pdf')
+
+    plot.xlim(dataframe.index[0], datetime.fromtimestamp(time.time()+360))
 
 '''----------------------------------------------------------------------------------------------'''
 
@@ -100,7 +103,33 @@ def des(dataframe, alpha, beta):
     fit = Holt(dataframe, exponential=True).fit(smoothing_level=alpha, smoothing_trend=beta, optimized=False)
     forecast = fit.forecast(2).rename("Exponential trend")
     forecast.plot(style='--', marker='o', color='red', legend=True)
-    plot.show()
+
+    plot.xlim(dataframe.index[0], datetime.fromtimestamp(time.time()+540))
+
+'''----------------------------------------------------------------------------------------------'''
+
+def create_graphs(client, attribute, alpha, beta):
+    
+    dataframe = influx2DataFrame(client, 'select ' + attribute + ' from speedtest')
+    print(dataframe)
+    dataframe.plot.line()
+
+    if beta == 0:
+        ses(dataframe, alpha)
+    else:
+        des(dataframe, alpha, beta)
+
+    plot.xlabel('Time')
+    if attribute=='ping':
+        plot.ylabel('Ping (ms)')
+    elif attribute=='download':
+        plot.ylabel('Download (Mbps)')
+    else:
+        plot.ylabel('Upload (Mbps)')
+
+    plot.grid(axis='both', which='both')
+    plot.savefig(f'{GRAPHICS_PATH}/{attribute}', format='pdf')
+
 
 '''----------------------------------------------------------------------------------------------'''
 
@@ -110,16 +139,10 @@ def main():
 
     args = parse_args()
     print(args)
-    times = args.test.pop(0)
+    times = args.test
     
     if times <= 0:
-        print("times deve essere un intero maggiore di zero")
-        exit(-1)
-
-    rate = args.test.pop(0)
-
-    if rate <= 0:
-        print("rate deve essere un intero maggiore di zero")
+        print('times deve essere un intero maggiore di zero')
         exit(-1)
 
     alpha = args.forecast.pop()
@@ -141,22 +164,26 @@ def main():
     client = InfluxDBClient(username=user, password=password, database=database_name)
 
     client.drop_measurement("speedtest")
-
+    
     for i in range(times):
         init_time = time.time()
+        print('Starting speedtest number ' + str(i+1))
         results = test()
         write2Influx(client, "speedtest", results)
         final_time = time.time()
-        time.sleep(rate - (final_time - init_time))
+        if i != (times-1):
+            time.sleep(180 - (final_time - init_time))
 
-    dataframe = influx2DataFrame(client, 'select ping from speedtest')
-    print(dataframe)
-    dataframe.plot.line()
-    if beta == 0:
-        ses(dataframe, alpha)
-    else:
-        des(dataframe, alpha, beta)
+    if not os.path.exists(GRAPHICS_PATH):
+        os.makedirs(GRAPHICS_PATH)
 
+    create_graphs(client, 'ping', alpha, beta)
+    create_graphs(client, 'download', alpha, beta)
+    create_graphs(client, 'upload', alpha, beta)
+
+    client.close()
+
+'''----------------------------------------------------------------------------------------------'''
 
 if __name__ == '__main__':
     main()
